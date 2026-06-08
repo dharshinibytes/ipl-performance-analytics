cb63255c9acf0ea2d261a0257e59b80a86f8f57b
dharshinibytes <dharshinippriyas@gmail.com>
2026-06-08 20:03:13 +0530

    Add sql file

---
+++ SQLQuery2.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Deliveries (
+		match_id INT  ,
+		inning INT,
+		batting_team VARCHAR(50),
+		bowling_team VARCHAR(50),
+		[over] INT,
+		ball INT,
+		batter VARCHAR (50),
+		bowler VARCHAR(50),
+		non_striker VARCHAR(50),
+		batsman_runs int,
+		extra_runs INT,
+		total_runs INT,
+		extras_type VARCHAR(50),
+		is_wicket INT,
+		player_dismissed VARCHAR(50),
+		dismissal_kind VARCHAR(50),
+		fielder VARCHAR(50)
+);
